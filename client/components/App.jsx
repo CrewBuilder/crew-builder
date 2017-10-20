4a06c187d683b61876f0b3f251eb2c30fdeeb23c
Zachary Biernat <zackbiernat@gmail.com>
2017-10-20 16:44:03 -0700

    Setup React app to render, added button

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,1 +1,45 @@
-const a = 1;
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FacebookLogin from 'react-facebook-login';
+
+const responseFacebook = (response) => {
+  console.log(response);
+}
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.checkLogin = () => {
+      FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+        console.log('response',response);
+      });
+
+
+    }
+
+  }
+
+  render() {
+    function checkLoginState() {
+      FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+      });
+    }
+    function componentClicked(e) {
+      console.log('e',e)
+    }
+    return(
+      <div>
+        <FacebookLogin
+          appId="356644548109752"
+          autoLoad={true}
+          fields="name,email,picture"
+          onClick={componentClicked}
+          callback={responseFacebook} />,
+      </div>
+    )
+  }
+}
+
