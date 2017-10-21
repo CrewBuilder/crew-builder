d010c574c6208d7aa19cd7da412abbd61aca1086
meghanasarikonda <meghana94987@gmail.com>
2017-10-22 05:05:03 +0530

    pull the schemas

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,8 @@
 const sequelize = require('sequelize');
 require('dotenv').config();
+const Crew = require('./models/Crew');
+const User = require('./models/User');
+
 
 const db = new sequelize('crewbuilder', process.env.DB_USER, process.env.DB_PASSWORD, {
   host: 'localhost',
@@ -20,5 +23,8 @@ db
   .catch(err => {
     console.error('Unable to connect to the database:', err);
   });
+
+User.User.belongsToMany(Crew.Crew, {through: 'UserCrew'})
+Crew.Crew.belongsToMany(User.User, {through: 'UserCrew'})
 
 exports.db = db;
