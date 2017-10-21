04db2c379a9ce3fd8a8ff484d882e6a03d7ee0bb
Zachary Biernat <zackbiernat@gmail.com>
2017-10-20 20:09:51 -0700

    Added some passport to server

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,41 +3,40 @@ import ReactDOM from 'react-dom';
 import FacebookLogin from 'react-facebook-login';
 
 const responseFacebook = (response) => {
-  console.log(response);
+  //TODO: re-direct
 }
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.checkLogin = () => {
-      FB.getLoginStatus(function(response) {
-        statusChangeCallback(response);
-        console.log('response',response);
-      });
-
-
     }
 
-  }
-
   render() {
-    function checkLoginState() {
+    function checkLoginState(e) {
+      e.preventDefault()
       FB.getLoginStatus(function(response) {
         statusChangeCallback(response);
       });
     }
     function componentClicked(e) {
-      console.log('e',e)
+      console.log('Clicked login!');
+    }
+    function logoutClickHandler(e) {
+      e.preventDefault()
+      FB.logout(function(response) {
+        console.log('logged out', response);
+      });
     }
     return(
       <div>
         <FacebookLogin
-          appId="356644548109752"
+          appId="356644548109752" //TODO: hide this potentially sensitive info
           autoLoad={true}
-          fields="name,email,picture"
+          fields="name,email,picture" // TODO: subject to change if different fields are required
           onClick={componentClicked}
-          callback={responseFacebook} />,
+          callback={responseFacebook} />
+        <button onClick={logoutClickHandler}>Logout</button>
       </div>
     )
   }
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const FacebookStrategy = require('passport-facebook').Strategy;
+
 
 // INIT APP
 const app = express();
@@ -13,6 +16,35 @@ app.use(bodyParser.json());
 app.get('*', (req, res) => {
   res.send('Server Running');
 });
+
+// PASSPORT START
+passport.serializeUser(function(user, done) {
+  console.log('User profile has been received and is:', user );
+  //TODO: A function which tests if user is in db by email
+  //handleUserDataFacebook(user)
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.use(new FacebookStrategy({
+    clientID: '356644548109752',
+    clientSecret: '52ad76caab8dd22fbece177eb65aa942',
+    callbackURL: "/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    // TODO: Add CRUD database command here
+    //                                   |
+    //                                   v
+    // User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+    //   return cb(err, user);
+    // });
+  }
+));
+
+// PASSPORT END
 
 // CHECK PORT AND START SERVER
 const port = process.env.PORT || 3000;
