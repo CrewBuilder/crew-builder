09c0cae3213de502bcb1980b85f3e463ba2868a0
meghanasarikonda <meghana94987@gmail.com>
2017-10-21 08:05:01 +0530

    authenticate function connect to db

--- server/db/index.js
+++ server/db/index.js
@@ -11,4 +11,13 @@ const db = new sequelize('crewbuilder', 'meghanasarikonda', '', {
   }
 });
 
+db
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
 exports = db;
