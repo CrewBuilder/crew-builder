278423446c75ed05574aac36928ba348f1a91ccc
meghanasarikonda <meghana94987@gmail.com>
2017-10-22 00:52:16 +0530

    require dotenv

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const FacebookStrategy = require('passport-facebook').Strategy;
-
+require('dotenv').config();
 
 // INIT APP
 const app = express();
