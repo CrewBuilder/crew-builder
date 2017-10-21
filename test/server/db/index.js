029e73f4c0f61bf908dab081c4055864a8305793
ionajewel <ipjwilli@gmail.com>
2017-10-21 15:59:13 -0700

    basic tests for db connection and modified npm test script to recursive

--- test/server/db/index.js
+++ test/server/db/index.js
@@ -5,75 +5,19 @@ const expect = require('chai').expect;
 describe('Postgres crewbuilder db', () => {
   var dbConnection;
 
-  beforeEach((done) => {
+  beforeEach(function(done) {
     dbConnection = new sequelize('crewbuilder', process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'postgres',
     });
+    done();
   });
 
   afterEach(function() {
-    //dbConnection.end();
+    dbConnection.sync({force: true});
   });
 
-  it('Should insert posted messages to the DB', function(done) {
-    // Post the user to the chat server.
-    request({
-      method: 'POST',
-      uri: 'http://127.0.0.1:3000/classes/users',
-      json: { username: 'Valjean' }
-    }, function () {
-      // Post a message to the node chat server:
-      request({
-        method: 'POST',
-        uri: 'http://127.0.0.1:3000/classes/messages',
-        json: {
-          username: 'Valjean',
-          message: 'In mercy\'s name, three days is all I need.',
-          roomname: 'Hello'
-        }
-      }, function () {
-        // Now if we look in the database, we should find the
-        // posted message there.
-
-        // TODO: You might have to change this test to get all the data from
-        // your message table, since this is schema-dependent.
-        var queryString = 'SELECT * FROM messages';
-        var queryArgs = [];
-
-        dbConnection.query(queryString, queryArgs, function(err, results) {
-          // Should have one result:
-          expect(results.length).to.equal(1);
-
-          // TODO: If you don't have a column named text, change this test.
-          expect(results[0].text).to.equal('In mercy\'s name, three days is all I need.');
-
-          done();
-        });
-      });
-    });
-  });
-
-  it('Should output all messages from the DB', function(done) {
-    // Let's insert a message into the db
-
-    var queryString = 'INSERT INTO messages(text, userid, roomname) VALUES (?, ?, ?)';
-    var queryArgs = ['Men like you can never change!', 1, 'main'];
-    // TODO - The exact query string and query args to use
-    // here depend on the schema you design, so I'll leave
-    // them up to you. */
-
-    dbConnection.query(queryString, queryArgs, function(err) {
-      if (err) { throw err; }
-
-      // Now query the Node chat server and see if it returns
-      // the message we just inserted:
-      request('http://127.0.0.1:3000/classes/messages', function(error, response, body) {
-        var messageLog = JSON.parse(body);
-        expect(messageLog[0].text).to.equal('Men like you can never change!');
-        expect(messageLog[0].roomname).to.equal('main');
-        done();
-      });
-    });
+  it('Should connect to the crewbuilder database', function() {
+    return dbConnection.authenticate();
   });
 });
