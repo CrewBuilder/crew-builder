cb50bef11fe815438793b70bc7d32dfa78ef90f3
meghanasarikonda <meghana94987@gmail.com>
2017-10-22 15:09:35 +0530

    use promiseall

---
+++ server/db/models/associations.js
@@ -0,0 +1,20 @@
+const db = require('../index.js');
+const sequelize = require('sequelize')
+const Users = require('./Users');
+const Crews = require('./Crew');
+
+const Users_Crews = db.define('users_crews', {});
+
+Promise.all([Users, Crews])
+  .then(values => {
+    Users.belongsToMany(Crews, {through: Users_Crews, foreignKey: 'userId'});
+    Crews.belongsToMany(Users, {through: Users_Crews, foreignKey: 'crewId'});
+  })
+  .catch(error => {
+    console.log('error!')
+  })
+
+
+db.sync({
+  force: true
+});
