c095a0a82dcfcb71961d42c819ea662da2bb87ea
meghanasarikonda <meghana94987@gmail.com>
2017-10-22 15:11:28 +0530

    handle asscocations in seperate file

--- server/db/index.js
+++ server/db/index.js
@@ -1,8 +1,6 @@
 const sequelize = require('sequelize');
 require('dotenv').config();
-const Crew = require('./models/Crew');
-const User = require('./models/User');
-
+// console.log('Crew------>', Crew)
 
 const db = new sequelize('crewbuilder', process.env.DB_USER, process.env.DB_PASSWORD, {
   host: 'localhost',
@@ -24,7 +22,7 @@ db
     console.error('Unable to connect to the database:', err);
   });
 
-User.User.belongsToMany(Crew.Crew, {through: 'UserCrew'})
-Crew.Crew.belongsToMany(User.User, {through: 'UserCrew'})
+// User.User.belongsToMany(Crew.Crew, {through: 'UserCrew'})
+// Crew.Crew.belongsToMany(User.User, {through: 'UserCrew'})
 
-exports.db = db;
+module.exports = db;
