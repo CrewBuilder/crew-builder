c6b6481eddece1a4c69e4725a71ea4a980496448
ionajewel <ipjwilli@gmail.com>
2017-10-23 12:01:33 -0700

    crews mock data created

---
+++ test/server/db/seed/crews.js
@@ -0,0 +1,36 @@
+module.exports = [
+  {
+    crewId: 1,
+    name: 'Strings Attached',
+    decription: 'I started Strings Attached as a genre-blurring collaboration with folk artists. Our vision was to fuse jazz and classical flavors with the contemporary singer/songwriter genre; to dress it up with a little different jewelry. From the classical tradition we borrowed the architectural precision of composition and arranging. From jazz we brought the performance ethic. The ability to abandon the score and make choices spontaneously, in response to each other and the present musical moment. And then there\'s that irresistable sense of"swing" - the thing that gets people dancing.',
+    image: 'http://www.celebratewithstringsattached.com/western-swing.html'
+  },
+  {
+    crewId: 2,
+    name: 'tincidunt aliquam arcu.',
+    description: 'eleifend non, dapibus rutrum, justo. Praesent luctus. Curabitur egestas nunc sed libero. Proin sed turpis nec mauris blandit mattis. Cras eget nisi',
+    image: 'nec'
+  },
+  {
+    crewId: 3,
+    name: 'urna. Nunc',
+    description: 'dignissim tempor arcu. Vestibulum ut eros non enim commodo hendrerit. Donec porttitor tellus non magna. Nam ligula elit, pretium et, rutrum non, hendrerit id, ante. Nunc mauris sapien, cursus in, hendrerit consectetuer, cursus et, magna. Praesent interdum ligula eu enim. Etiam',
+    image: 'non,'
+  },
+  {
+    crewId: 4,
+    name: 'adipiscing non,',
+    description: 'porttitor interdum. Sed auctor odio a purus. Duis elementum, dui quis accumsan convallis, ante lectus convallis est, vitae sodales nisi magna sed dui. Fusce aliquam, enim nec tempus scelerisque, lorem ipsum sodales purus, in molestie tortor nibh sit amet orci. Ut',
+    image: 'Integer'},
+  {
+    crewId: 5,
+    name: 'vestibulum',
+    description: 'sit amet ultricies sem magna nec quam. Curabitur vel lectus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur',
+    image: 'risus.'},
+  {
+    crewId: 6,
+    name: 'a',
+    description: 'massa rutrum magna. Cras convallis convallis dolor. Quisque tincidunt pede ac urna. Ut tincidunt vehicula risus. Nulla eget metus eu erat semper rutrum. Fusce dolor quam, elementum at, egestas a, scelerisque',
+    image: 'sit'
+  },
+];
--- test/server/db/seed/users.js
+++ test/server/db/seed/users.js
@@ -1,17 +1,22 @@
 module.exports.userData = [
   {
-    user
+    userID: 'I8bL1aQKE2cr3jrna6geEpf4bWZ2',
+    facebook: '{"DISPLAY_NAME": "ionajewel", "EMAIL": "ipjwilli@gmail.com", "IMAGE_URL": "https://lh3.googleusercontent.com/-XdUIqdMkCWA/AAAAAAAAAAI/AAAAAAAAAAA/4252rscbv5M/photo.jpg"}'
   },
   {
-
+    userID: 'Wzi9EQaXcbgnbyjwWYtVBTvOeN33',
+    facebook: '{"DISPLAY_NAME": "johndoe", "EMAIL": "johndoe@johndoe.com", "IMAGE_URL": "https://lh3.googleusercontent.com/-XdUIqdMkCWA/AAAAAAAAAAI/AAAAAAAAAAA/4252rscbv5M/photo.jpg}'
   },
   {
-
+    userID: 'drlnGKwWknYALR1rA45apQlF6y13',
+    facebook: '{"DISPLAY_NAME": "janesmith", "EMAIL": "janesmith@janesmith.com", "IMAGE_URL": "https://lh3.googleusercontent.com/-XdUIqdMkCWA/AAAAAAAAAAI/AAAAAAAAAAA/4252rscbv5M/photo.jpg}'
   },
   {
-
+    userID: 'z8GjWJYn9TdY5rKEI4cpsQb9Sdq2',
+    facebook: '{"DISPLAY_NAME": "allanjohnson", "EMAIL": "allanjohnson@allanjohnson.com", "IMAGE_URL": "https://lh3.googleusercontent.com/-XdUIqdMkCWA/AAAAAAAAAAI/AAAAAAAAAAA/4252rscbv5M/photo.jpg}'
   },
   {
-
+    userID: 'w2ljLy2P9gNbdh6xF0tVgtTf49u1',
+    facebook: '{"DISPLAY_NAME": "ivannawynn", "EMAIL": "ivannawynn@ivannawynn.com", "IMAGE_URL": "https://lh3.googleusercontent.com/-XdUIqdMkCWA/AAAAAAAAAAI/AAAAAAAAAAA/4252rscbv5M/photo.jpg}'
   }
 ];
