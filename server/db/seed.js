48455322ddb26853f461eeea2704902f8dad4108
ionajewel <ipjwilli@gmail.com>
2017-10-23 20:23:23 -0700

    db test running, just need to correct assert statement

--- server/db/seed.js
+++ server/db/seed.js
@@ -13,67 +13,67 @@ const TaskSeed = require('../../test/server/db/seed/task.js');
 const UserCrewSeed = require('../../test/server/db/seed/user_crew.js');
 const UserTaskSeed = require('../../test/server/db/seed/user_task.js');
 
-db.drop()
-  .then(() => {
-    console.log('Database dropped.');
-    db.sync()
+module.exports = (function() {
+  db.drop()
+    .then(() => {
+      console.log('Database dropped.');
+      db.sync()
 
-      .then(() => { //seed users table
-        UserSeed.forEach(user => {
-          User.create({
-            facebook: user.facebook
-          }).then(() => console.log('Users created')).catch(err => console.log('error seeding Users', err));
+        .then(() => { //seed users table
+          UserSeed.forEach(user => {
+            User.create({
+              facebook: user.facebook
+            }).then(() => console.log('Users created')).catch(err => console.log('error seeding Users', err));
+          });
+        })
+
+        .then(() => { //seed crews table
+          CrewSeed.forEach(crew => {
+            Crew.create({
+              name: crew.name,
+              description: crew.description,
+              image: crew.image
+            }).then(() => console.log('Crews created')).catch(err => console.log('error seeding Crews', err));
+          });
+        })
+
+        .then(() => { //seed tasks
+          TaskSeed.forEach(task => {
+            Task.create({
+              name: task.name,
+              description: task.description,
+              points: task.points,
+              limit: task.limit,
+              expiry: task.expiry,
+              crewId: task.crewId
+            }).then(() => console.log('Task created')).catch(err => console.log('error seeding Tasks', err));
+          });
+        })
+
+        .then(() => { //seed users_crews
+          UserCrewSeed.forEach(userCrew => {
+            UserCrew.create({
+              userId: userCrew.userId,
+              crewId: userCrew.crewId,
+              points: userCrew.points,
+              achievement: userCrew.achievement,
+              role: userCrew.role
+            }).then(() => console.log('UserCrew created')).catch(err => console.log('error seeding UserCrew', err));
+          });
+        })
+
+        .then(() => { //seed users_crews
+          UserTaskSeed.forEach(userTask => {
+            UserTask.create({
+              userId: userTask.userId,
+              taskId: userTask.taskId,
+              completed: userTask.completed,
+              verified: userTask.verified,
+            }).then(() => console.log('UserTask created')).catch(err => console.log('error seeding UserTask', err));
+          });
+        })
+        .catch(err => {
+          console.log('Databse did not sync: ', err);
         });
-      })
-
-      .then(() => { //seed crews table
-        CrewSeed.forEach(crew => {
-          Crew.create({
-            name: crew.name,
-            description: crew.description,
-            image: crew.image
-          }).then(() => console.log('Crews created')).catch(err => console.log('error seeding Crews', err));
-        });
-      })
-
-      .then(() => { //seed tasks
-        TaskSeed.forEach(task => {
-          Task.create({
-            name: task.name,
-            description: task.description,
-            points: task.points,
-            limit: task.limit,
-            expiry: task.expiry,
-            crewId: task.crewId
-          }).then(() => console.log('Task created')).catch(err => console.log('error seeding Tasks', err));
-        });
-      })
-
-
-
-      .then(() => { //seed users_crews
-        UserCrewSeed.forEach(userCrew => {
-          UserCrew.create({
-            userId: userCrew.userId,
-            crewId: userCrew.crewId,
-            points: userCrew.points,
-            achievement: userCrew.achievement,
-            role: userCrew.role
-          }).then(() => console.log('UserCrew created')).catch(err => console.log('error seeding UserCrew', err));
-        });
-      })
-
-      .then(() => { //seed users_crews
-        UserTaskSeed.forEach(userTask => {
-          UserTask.create({
-            userId: userTask.userId,
-            taskId: userTask.taskId,
-            completed: userTask.completed,
-            verified: userTask.verified,
-          }).then(() => console.log('UserTask created')).catch(err => console.log('error seeding UserTask', err));
-        });
-      })
-      .catch(err => {
-        console.log('Databse did not sync: ', err);
-      });
-  });
+    });
+}) ();
--- test/server/db/index.js
+++ test/server/db/index.js
@@ -1,12 +1,18 @@
-const sequelize = require('sequelize');
+const Sequelize = require('sequelize');
 require('dotenv').config();
 const expect = require('chai').expect;
+const User = require('../../../server/db/models/User.js');
+const Crew = require('../../../server/db/models/Crew.js');
+const Task = require('../../../server/db/models/Task.js');
+const UserCrew = require('../../../server/db/models/User_Crew.js');
+const UserTask = require('../../../server/db/models/User_Task.js');
 
 describe('Postgres crewbuilder db', function() {
-  var dbConnection;
+  var db;
 
   beforeEach(function(done) {
-    dbConnection = new sequelize('crewbuilder', process.env.DB_USER, process.env.DB_PASSWORD, {
+    //connect to (presumably seeded) database 'crewbuilder'
+    db = new Sequelize('crewbuilder', process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'postgres',
     });
@@ -14,10 +20,34 @@ describe('Postgres crewbuilder db', function() {
   });
 
   afterEach(function() {
-    dbConnection.sync({force: true});
   });
 
   it('Should connect to the crewbuilder database', function() {
-    return dbConnection.authenticate();
+    return db.authenticate();
+  });
+
+  it('Should create a new user if facebook id is not already in the db', function(done) {
+    let profile = '{"DISPLAY_NAME": "maryjane", "EMAIL": "maryjane@maryjane.com", "IMAGE_URL": "https://lh3.googleusercontent.com/-XdUIqdMkCWA/AAAAAAAAAAI/AAAAAAAAAAA/4252rscbv5M/photo.jpg", "FACEBOOK_ID": "123456"}';
+    profile = JSON.parse(profile);
+
+    User.findOne(
+      {
+        where: {
+          facebook: {
+            FACEBOOK_ID: profile.FACEBOOK_ID
+          }
+        }
+      })
+      .then(function(user) {
+        if (!user) {
+          // No user found...let's create one
+          User.create({ facebook: JSON.stringify(profile) })
+            .then(user => {
+              console.log('User created');
+              expect(user.facebook).to.equal(profile);
+            });
+        }
+      });
+    done();
   });
 });
