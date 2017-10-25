07d2973834570f6852904995ca4170c248044dfb
Zachary Biernat <zackbiernat@gmail.com>
2017-10-25 16:03:53 -0700

    Outlined CrewView with sample data

--- client/components/dashboard/main/crewView/crewView.jsx
+++ client/components/dashboard/main/crewView/crewView.jsx
@@ -3,21 +3,70 @@ import React, { Component } from 'react';
 import CrewSummary from './crewSummary.jsx';
 import TasksInProgress from './tasksInProgress.jsx';
 import TasksAvailable from './tasksAvailable.jsx';
+// Compares userTasks to crewTasks => sends to tasksInProgress and tasksAvailable respectively
+
+
+// Example data
+const crewData = {
+  "name": 'Strings Attached',
+  "decription": 'I started Strings Attached as a genre-blurring collaboration with folk artists. Our vision was to fuse jazz and classical flavors with the contemporary singer/songwriter genre; to dress it up with a little different jewelry. From the classical tradition we borrowed the architectural precision of composition and arranging. From jazz we brought the performance ethic. The ability to abandon the score and make choices spontaneously, in response to each other and the present musical moment. And then there\'s that irresistable sense of"swing" - the thing that gets people dancing.',
+  "image": 'http://www.celebratewithstringsattached.com/western-swing.html'
+};
+
+const userIdData = '1';
+
+const tasksInProgress = [{
+  "name": "Bergstrom, Blanda and Hamill",
+  "description": "Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.",
+  "points": 86,
+  "expiry": "2017-09-14T09:32:33Z",
+  "completed": false,
+  "verified": false
+}, {
+  "name": "Zemlak, Koepp and Schoen",
+  "description": "Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat. In congue. Etiam justo. Etiam pretium iaculis justo. In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.",
+  "points": 13,
+  "expiry": "2017-06-01T01:07:43Z",
+  "completed": true,
+  "verified": false
+}, {
+  "name": "Waters and Sons",
+  "description": "Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante.",
+  "points": 42,
+  "expiry": "2017-03-05T22:58:30Z",
+  "completed": true,
+  "verified": true
+}];
+
+const crewTaskAvailableData = [{
+  "name": "Jacobi-Schiller",
+  "description": "Duis at velit eu est congue elementum. In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo. Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.",
+  "points": 81,
+  "expiry": "2017-07-21T08:19:26Z",
+}, {
+  "name": "Heller Inc",
+  "description": "Morbi quis tortor id nulla ultrices aliquet. Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui. Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti. Nullam porttitor lacus at turpis.",
+  "points": 13,
+  "expiry": "2017-06-11T15:21:19Z",
+}];
 
 
 export default class CrewView extends Component {
 
   constructor(props) {
     super(props);
+    // Needed props: currentCrew, user
+
+    // this.getCrewTasks
+    // gets tasks from crewId
   }
 
   render() {
     return (
       <div>
-        <CrewSummary />
-        <TasksInProgress />
-        <TasksAvailable />
-        <p>Crew view COMPONENT</p>
+        <CrewSummary userId={userIdData} crew={crewData} />
+        <TasksInProgress userId={userIdData} userTasks={tasksInProgress} />
+        <TasksAvailable userId={userIdData} availableTasks={crewTaskAvailableData} />
       </div>
     )
   }
