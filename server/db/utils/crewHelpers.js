7b497cf9b6efe83b01cfee3f4164c982754c5c3d
ionajewel <ipjwilli@gmail.com>
2017-10-25 11:54:53 -0700

    crew helper queries tested OK

--- server/db/utils/crewHelpers.js
+++ server/db/utils/crewHelpers.js
@@ -1,41 +1,44 @@
-let Crew = require('./../models/Crew.js');
-
+const db = require('../index.js');
 // Returns one crew for single id
 //currently unused
 exports.findCrewById = (id, cb) => {
-  Crew.findById(id)
-  .then(crew => {
-    cb(null, crew);
-  })
-  .catch(err => {
-    cb(err, null);
-  })
-}
+  //this query has been tested OK
+  db.Crew.findById(id)
+    .then(crew => {
+      cb(null, crew);
+    })
+    .catch(err => {
+      cb(err, null);
+    });
+};
 
 // Returns all crews for array of id(s). Used by '/user/crews' endpoint in config.js.
 exports.findAllCrewsByIds = (ids, cb) => {
-  Crews.findAll({
+  //this query has been test OK
+  db.Crew.findAll({
     where: {
       id: {
         $in: ids
       }
     }
   })
-  .then(crewsData => {
-    if (!crewsData.length) {
-      cb(err, null);
-    } else {
-      cb(null, crewsData);
-    }
-  })
-}
+    .then(crewsData => {
+      if (!crewsData.length) {
+        cb(err, null);
+      } else {
+        cb(null, crewsData);
+      }
+    });
+};
 
 // Allows crew creation
 exports.postCrew = (crewData, cb) => {
-  Crew.create(crewData)
-  .then(crew => {
-    console.log('Created a new task', crew);
-    // Callback with crew data
-    return cb(null, crew);
-  })
-}
+  //this query has been tested OK
+  db.Crew.create(crewData)
+    .then(crew => {
+      return cb(null, crew);
+    })
+    .catch(err => {
+      return cb(err, null);
+    });
+};
--- test/server/db/index.js
+++ test/server/db/index.js
@@ -51,6 +51,7 @@ describe('Postgres crewbuilder db', function() {
           done();
         }
       });
+
   });
 
   it('Should associate a user with crews via the users_crews join table', function(done) {
@@ -64,6 +65,51 @@ describe('Postgres crewbuilder db', function() {
         done();
       })
       .catch(function(err) {
+        done(err);
+      });
+  });
+
+  it('Should create a new crew by id', function(done) {
+    db.Crew.findById(1)
+      .then(crew => {
+        expect(crew.id).to.equal(1);
+        done();
+      })
+      .catch(err => {
+        done(err);
+      });
+  });
+
+  it('Should return all crews given an array of ids', function(done) {
+    db.Crew.findAll({
+      where: {
+        id: {
+          $in: [1, 2, 3, 4]
+        }
+      }
+    })
+      .then(crewsData => {
+        if (!crewsData.length) {
+          done(err);
+        } else {
+          expect(crewsData.length).to.equal(4);
+          done();
+        }
+      });
+  });
+
+  it('Should create a new crew', function(done) {
+    let crewData = {
+      "name": "Christiansen, Grimes and Rosenbaum",
+      "description": "Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est. Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum. Proin eu mi. Nulla ac enim.",
+      "image": "http://dummyimage.com/160x231.jpg/cc0000/ffffff"
+    };
+    db.Crew.create(crewData)
+      .then(crew => {
+        expect(crew.name).to.equal(crewData.name);
+        done();
+      })
+      .catch(err => {
         done(err);
       });
   });
