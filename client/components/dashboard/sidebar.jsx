b51c8449fc560a93a1d54b3054013e882b462e4a
Zachary Biernat <zackbiernat@gmail.com>
2017-10-25 12:34:49 -0700

    Added sample data for side bar

--- client/components/dashboard/sidebar.jsx
+++ client/components/dashboard/sidebar.jsx
@@ -5,19 +5,46 @@ export default class Sidebar extends Component {
   constructor(props) {
     super(props);
     // Expect 'props' to contain 'user', 'userCrews', and 'handleCrewClick' function which sets current crew in main view
+    let userData = {
+      facebookId: '1',
+      facebook: {
+        DISPLAY_NAME: "ionajewel",
+        EMAIL: "ipjwilli@gmail.com",
+        IMAGE_URL: "https://lh3.googleusercontent.com/-XdUIqdMkCWA/AAAAAAAAAAI/AAAAAAAAAAA/4252rscbv5M/photo.jpg"
+      }
+    };
+    let crewData = [{
+      "name": 'Strings Attached',
+      "decription": 'I started Strings Attached as a genre-blurring collaboration with folk artists. Our vision was to fuse jazz and classical flavors with the contemporary singer/songwriter genre; to dress it up with a little different jewelry. From the classical tradition we borrowed the architectural precision of composition and arranging. From jazz we brought the performance ethic. The ability to abandon the score and make choices spontaneously, in response to each other and the present musical moment. And then there\'s that irresistable sense of"swing" - the thing that gets people dancing.',
+      "image": 'http://www.celebratewithstringsattached.com/western-swing.html'
+    },
+    {
+      "name": "Zieme, Nitzsche and Murazik",
+      "description": "Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio. Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo.",
+      "image": "http://dummyimage.com/201x122.jpg/ff4444/ffffff"
+    }];
+    this.state = {
+      user: userData,
+      userCrews: crewData
+    }
+
+    this.handleCrewClick = (val, e) => {
+      e.preventDefault();
+      console.log(val);
+    }
   }
 
   render() {
     return (
       <div>
         <div className="sidebar-crew-list">
-          {this.props.userCrews.map((crew, i) => {
+          {this.state.userCrews.map((crew, key) => {
             return (
-              <div onClick={this.props.handleCrewClick} className="sidebar-crew-name">{crew.name}</div>
+              <div onClick={e => this.handleCrewClick(crew, e)} value={crew.name} key={key} className="sidebar-crew-name">{crew.name}</div>
             )
           })}
         </div>
-        {this.props.userCrews.length === 0 ?
+        {this.state.userCrews.length === 0 ?
           <div className="no-crews-message">
             <h2><em>
               You don't have any crews! Use the searchbar to find one.
