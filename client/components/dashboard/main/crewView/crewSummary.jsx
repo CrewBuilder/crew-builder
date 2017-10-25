2469ef06a2df254d4b942882244b1873471d792d
Zachary Biernat <zackbiernat@gmail.com>
2017-10-25 15:22:13 -0700

    Added some skeleton to crewView component/subcomponenets

--- client/components/dashboard/main/crewView/crewSummary.jsx
+++ client/components/dashboard/main/crewView/crewSummary.jsx
@@ -1,1 +1,17 @@
 // This component renders a crewSummary card with some information and a picture
+import React, { Component } from 'react';
+
+export default class CrewSummary extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Crew summary view</p>
+      </div>
+    )
+  }
+}
--- client/components/dashboard/main/crewView/tasksInProgress.jsx
+++ client/components/dashboard/main/crewView/tasksInProgress.jsx
@@ -1,1 +1,17 @@
 // This component renders all the tasks a user has claimed for their crew
+import React, { Component } from 'react';
+
+export default class TasksInProgress extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <p>TasksInProgress view</p>
+      </div>
+    )
+  }
+}
