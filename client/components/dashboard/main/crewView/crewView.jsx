70a12cdf73e68cb75050a6f6cc2240a7e78a490d
ionajewel <ipjwilli@gmail.com>
2017-10-26 10:55:48 -0700

    improved mock data, added modal to available tasks

--- client/components/dashboard/main/crewView/crewView.jsx
+++ client/components/dashboard/main/crewView/crewView.jsx
@@ -3,51 +3,51 @@ import React, { Component } from 'react';
 import CrewSummary from './crewSummary.jsx';
 import TasksInProgress from './tasksInProgress.jsx';
 import TasksAvailable from './tasksAvailable.jsx';
-import {Panel, PanelGroup} from 'react-bootstrap'
+import {Panel, PanelGroup} from 'react-bootstrap';
 // Compares userTasks to crewTasks => sends to tasksInProgress and tasksAvailable respectively
 
 
 // Example data
 const crewData = {
   "name": 'Strings Attached',
-  "decription": 'I started Strings Attached as a genre-blurring collaboration with folk artists. Our vision was to fuse jazz and classical flavors with the contemporary singer/songwriter genre; to dress it up with a little different jewelry. From the classical tradition we borrowed the architectural precision of composition and arranging. From jazz we brought the performance ethic. The ability to abandon the score and make choices spontaneously, in response to each other and the present musical moment. And then there\'s that irresistable sense of"swing" - the thing that gets people dancing.',
-  "image": 'http://dummyimage.com/201x122.jpg/44ff44/ffffff'
+  "decription": 'Our vision is to fuse jazz and classical flavors with the contemporary singer/songwriter genre. From the classical tradition we borrowed the precision of composition and arranging. From jazz we brought the performance ethic. The ability to abandon the score and make choices spontaneously. And then there\'s that irresistable sense of"swing" - the thing that gets people dancing.',
+  "image": 'http://www.celebratewithstringsattached.com/western-swing.html'
 };
 
 const userIdData = '1';
 
 const tasksInProgress = [{
-  "name": "Bergstrom, Blanda and Hamill",
-  "description": "Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.",
-  "points": 86,
+  "name": "Instagram Testimonial",
+  "description": "Post a testimonial video to instgram with the hashtag #StringsAttached.",
+  "points": 50,
   "expiry": "2017-09-14T09:32:33Z",
   "completed": false,
   "verified": false
 }, {
-  "name": "Zemlak, Koepp and Schoen",
-  "description": "Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat. In congue. Etiam justo. Etiam pretium iaculis justo. In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.",
-  "points": 13,
+  "name": "Volunteer at Strings on Grass",
+  "description": "Arrive 1 hr prior to show time and/or stay 1 hr after show time to help set up/break down",
+  "points": 300,
   "expiry": "2017-06-01T01:07:43Z",
   "completed": true,
   "verified": false
 }, {
-  "name": "Waters and Sons",
-  "description": "Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante.",
-  "points": 42,
+  "name": "5 Facebook Invites",
+  "description": "Send Facebook invites to 5 people.",
+  "points": 50,
   "expiry": "2017-03-05T22:58:30Z",
   "completed": true,
   "verified": true
 }];
 
 const crewTaskAvailableData = [{
-  "name": "Jacobi-Schiller",
-  "description": "Duis at velit eu est congue elementum. In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo. Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.",
-  "points": 81,
+  "name": "Post Flyers",
+  "description": "Post Strings Attached Flyers at 5 locations. Send photos with geolocation to band email for verification.",
+  "points": 200,
   "expiry": "2017-07-21T08:19:26Z",
 }, {
-  "name": "Heller Inc",
-  "description": "Morbi quis tortor id nulla ultrices aliquet. Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui. Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti. Nullam porttitor lacus at turpis.",
-  "points": 13,
+  "name": "Tweet about us!",
+  "description": "Tweet about Strings Attached 5 days in a row using hashtag #StringsAttached",
+  "points": 100,
   "expiry": "2017-06-11T15:21:19Z",
 }];
 
@@ -77,6 +77,6 @@ export default class CrewView extends Component {
           </Panel>
         </PanelGroup>
       </div>
-    )
+    );
   }
 }
--- client/components/dashboard/main/crewView/tasksAvailable.jsx
+++ client/components/dashboard/main/crewView/tasksAvailable.jsx
@@ -1,30 +1,60 @@
 // This component renders a list of tasks that the user has not undertaken yet
 import React, { Component } from 'react';
-import { Accordion, Panel } from 'react-bootstrap';
+import { Modal, ListGroup, ListGroupItem, Button } from 'react-bootstrap';
 
 export default class TasksAvailable extends Component {
 
   constructor(props) {
     super(props);
-    // will need to include task id in order to make modal work properly
     this.state = {
-      selectedTask: ''
+      selectedTask: props.availableTasks[0],
+      showModal: false
     };
 
     this.tasks = this.props.availableTasks;
 
     this.handleSelectTask = (task) => {
-      this.setState({selectedTask: task.name});
-      console.log('Available task selected: ', task.name);
+      this.setState({selectedTask: task});
+      this.setState({showModal: true});
     };
+
+    this.closeModal = () => {
+      this.setState({showModal: false});
+    };
+
+    this.claimTask = () => {
+      this.setState({showModal: false});
+      console.log('Send task to server: ', this.state.selectedTask.name);
+    };
+  }
+
+  componentDidMount() {
+    this.setState({selectedTask: this.tasks[0]});
   }
 
   render() {
     return (
       <div>
-        <Accordion>
-          {this.tasks.map((task, i) => <Panel header={task.name} onSelect={() => this.handleSelectTask(task)} eventKey={i} key={i}><p>{task.description}</p></Panel>)}
-        </Accordion>
+        <div>
+          <ListGroup>
+            {this.tasks.map((task, i) => <ListGroupItem onClick={() => this.handleSelectTask(task)} key={i}>{task.name}</ListGroupItem>)}
+          </ListGroup>
+        </div>
+        <div>
+          <Modal show={this.state.showModal} onHide={this.closeModal}>
+            <Modal.Header closeButton>
+              <Modal.Title>{this.state.selectedTask.name}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <h4>Points: {this.state.selectedTask.points}</h4>
+              <p>{this.state.selectedTask.description}</p>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button onClick={this.claimTask}>Claim this task</Button>
+              <Button onClick={this.closeModal}>Close</Button>
+            </Modal.Footer>
+          </Modal>
+        </div>
       </div>
     );
   }
