5a8525a9258ebb0c432c75dda6e95a19d0cb855d
ionajewel <ipjwilli@gmail.com>
2017-10-25 20:50:43 -0700

    tasks available rendering as a collapsable panel group

--- client/components/dashboard/main/crewView/tasksAvailable.jsx
+++ client/components/dashboard/main/crewView/tasksAvailable.jsx
@@ -1,17 +1,31 @@
 // This component renders a list of tasks that the user has not undertaken yet
 import React, { Component } from 'react';
+import { Accordion, Panel } from 'react-bootstrap';
 
 export default class TasksAvailable extends Component {
 
   constructor(props) {
     super(props);
+    // will need to include task id in order to make modal work properly
+    this.state = {
+      selectedTask: ''
+    };
+
+    this.tasks = this.props.availableTasks;
+
+    this.handleSelectTask = (task) => {
+      this.setState({selectedTask: task.name});
+      console.log('Available task selected: ', task.name);
+    };
   }
 
   render() {
     return (
       <div>
-        <p>Tasks available view</p>
+        <Accordion>
+          {this.tasks.map((task, i) => <Panel header={task.name} onSelect={() => this.handleSelectTask(task)} eventKey={i} key={i}><p>{task.description}</p></Panel>)}
+        </Accordion>
       </div>
-    )
+    );
   }
 }
