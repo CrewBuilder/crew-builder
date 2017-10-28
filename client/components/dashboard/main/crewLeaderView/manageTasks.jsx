3861fbcf527630bf65af94f3952ee4ab5040ca90
meghanasarikonda <meghana94987@gmail.com>
2017-10-28 19:32:42 +0530

    Import model and Addtask form

--- client/components/dashboard/main/crewLeaderView/manageTasks.jsx
+++ client/components/dashboard/main/crewLeaderView/manageTasks.jsx
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
-import { ListGroup, ListGroupItem } from 'react-bootstrap'
+import { ListGroup, ListGroupItem, Modal } from 'react-bootstrap';
+import AddTask from './../../forms/addTask.jsx';
 
 export default class ManageTasks extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      showModal: false
+    };
+
+    this.open = () => {
+      this.setState({
+        showModal: true
+      })
+    }
+
+    this.close = () => {
+      this.setState({
+        showModal: false
+      })
+    }
   }
 
   render() {
@@ -14,7 +30,16 @@ export default class ManageTasks extends Component {
           {this.props.userTasks.map((task, i) => (
             <ListGroupItem key={i}>{task.name}</ListGroupItem>
           )) }
+          <ListGroupItem onClick={this.open}>+ addTask</ListGroupItem>
         </ListGroup>
+        <Modal show={this.state.showModal} onHide={this.close}>
+          <Modal.Header closeButton>
+            Add a new Task
+          </Modal.Header>
+          <Modal.Body>
+
+          </Modal.Body>
+        </Modal>
       </div>
     )
   }
