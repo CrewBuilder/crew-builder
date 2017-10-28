b4e315056cab6bbacef253eb6bd7d744d691ff43
meghanasarikonda <meghana94987@gmail.com>
2017-10-28 18:01:33 +0530

    manageTasks component

---
+++ client/components/dashboard/main/crewLeaderView/manageTasks.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class ManageTasks extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1>Hello</h1>
+    )
+  }
+}
