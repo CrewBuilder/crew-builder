0b10672a4a04fb9120367f8d27820f20100a3b24
Zachary Biernat <zackbiernat@gmail.com>
2017-10-30 15:46:53 -0700

    Made some changes to searchCard

--- client/components/dashboard/main/searchView/searchCard.jsx
+++ client/components/dashboard/main/searchView/searchCard.jsx
@@ -1,28 +1,43 @@
 // Here we will populate a 'card' entry for Each crew in a search results.
 
 import React, { Component } from 'react';
-
 import { Media } from 'react-bootstrap';
+import { JoinACrew } from '../../../utils/requests.jsx';
 
-export default class Sidebar extends Component {
+export default class SearchView extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      user: '',
+      crew: null
+    };
+
+
     // Expect 'props' to contain 'crew' and 'handleCrewClick' function which sets current crew in main view
+
+  }
+
+  componentWillMount() {
+    this.setState({
+      user: this.props.user,
+      crew: this.props.crew,
+    });
+
   }
 
   render() {
     return (
       <Media>
         <Media.Left align="top">
-          <img width={124} height={124} src={this.props.crew.image} alt="placeholder thumbnail" />
+          <img width={124} height={124} src={this.props.crew.image} crew={this.state.crew} alt="placeholder thumbnail" onClick={(e, crew) => this.props.joinCrew(e, crew)} />
         </Media.Left>
         <Media.Body>
           <Media.Heading>{this.props.crew.name}</Media.Heading>
           <p>{this.props.crew.description}</p>
         </Media.Body>
       </Media>
-    )
+    );
   }
 }
 
