50bb99d45e9413cf4f375637a149fb372f7a2965
Zachary Biernat <zackbiernat@gmail.com>
2017-10-31 12:11:53 -0700

    Update dashboard.jsx

--- client/components/dashboard/dashboard.jsx
+++ client/components/dashboard/dashboard.jsx
@@ -122,7 +122,7 @@ export default class Dashboard extends Component {
       }).then((userId) => {
         // get user crews using userId.id
         let id = userId.id;
-        GetUserCrews(1, (err, res) => {
+        GetUserCrews(id, (err, res) => {
           if (err) {
             console.log('ERROR:', err);
           } else {
