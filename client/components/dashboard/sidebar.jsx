0071640d57d3296e6590f47787ef2efd02630ab8
Zachary Biernat <zackbiernat@gmail.com>
2017-11-04 17:27:15 -0700

    Improved sidebar menu for mobile

--- client/components/dashboard/sidebar.jsx
+++ client/components/dashboard/sidebar.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import { LinkContainer } from 'react-router-bootstrap';
 
-import { MenuItem, NavDropdown, NavBar, Row, Col, Nav, NavItem, Tab, TagContainer, TabContent, TabPane, Label, Button, Glyphicon } from 'react-bootstrap';
+import { MenuItem, NavDropdown, Navbar, Row, Col, Nav, NavItem, Tab, TagContainer, TabContent, TabPane, Label, Button, Glyphicon } from 'react-bootstrap';
 
 export default class Sidebar extends Component {
 
@@ -61,23 +61,29 @@ export default class Sidebar extends Component {
                 {this.props.userMemberCrews && this.props.userMemberCrews.length === 0 ? <p className="sidebar-empty-crews-msg"><Glyphicon glyph="info-sign" /> <em>Just getting started or looking to find that next crew for you? Try our browse or search features to find some crews to join today!</em></p> : null}
               </span>
             </Col>
-            <Col smHidden={true} mdHidden={true} lgHidden={true} className="phone-only" >
-                <h4 className="sidebar-heading">My Crews:</h4>
-                <Nav bsStyle="pills" stacked>
-                  {this.props.userLeaderCrews.map((container) => {
+          </Row>
+        </Tab.Container>
+        <Row>
+          <Col smHidden={true} mdHidden={true} lgHidden={true} className="phone-only" >
+            <Navbar inverse fixedTop={true} fluid={true}>
+              <Nav>
+                <NavDropdown eventKey={1} title="My Crews" id="basic-nav-dropdown">
+                  {this.props.userLeaderCrews.map((container, i) => {
                     return (
-                      <LinkContainer to={`/dashboard/manage/${container.crew.id}`} key={container.crew.id} onClick={e => this.setCurrentCrew(container)}>
-                        <NavItem activeKey={container.crew.id}
-                          value={container.crew.name}
-                          key={container.crew.id}
-                          className="sidebar-crew-name">
-                          {container.crew.name}
-                        </NavItem>
-                      </LinkContainer>
+                      <MenuItem componentClass="span" eventKey={i} key={i}>
+                        <LinkContainer componentClass="span" to={`/dashboard/manage/${container.crew.id}`} key={container.crew.id} onClick={e => this.setCurrentCrew(container)}>
+                          <NavItem componentClass="span" activeKey={container.crew.id}
+                            value={container.crew.name}
+                            key={container.crew.id}
+                            className="sidebar-crew-name">
+                            {container.crew.name}
+                          </NavItem>
+                        </LinkContainer>
+                      </MenuItem>
                     )
                   })}
 
-                  <LinkContainer to={`/dashboard/newcrew`} key='createCrew' >
+                  <LinkContainer componentClass="span" to={`/dashboard/newcrew`} key='createCrew' >
                     <NavItem activeKey='createCrew'
                       value='createCrew'
                       key='createCrew'
@@ -85,27 +91,29 @@ export default class Sidebar extends Component {
                       <Glyphicon glyph="plus" /> create new crew
                     </NavItem>
                   </LinkContainer>
-                </Nav>
-                <h4 className="sidebar-heading">Crews I Follow:</h4>
-                <Nav bsStyle="pills" stacked>
+                </NavDropdown>
+                <NavDropdown eventKey={2} title="Crews I Follow" id="basic-nav-dropdown">
                   {this.props.userMemberCrews.map((container, i) => {
                     return (
-                      <LinkContainer to={`/dashboard/crews/${container.crew.id}`} key={i} onClick={e => this.props.setCurrentCrew(container, e)}>
-                        <NavItem activeKey={container.crew.id}
-                          value={container.crew.name}
-                          key={container.crew.id}
-                          className="sidebar-crew-name">
-                          {container.crew.name}
-                        </NavItem>
-                      </LinkContainer>
+                      <MenuItem componentClass="span" eventKey={i} key={i}>
+                        <LinkContainer componentClass="span" to={`/dashboard/crews/${container.crew.id}`} key={i} onClick={e => this.props.setCurrentCrew(container, e)}>
+                          <NavItem componentClass="span" activeKey={container.crew.id}
+                            value={container.crew.name}
+                            key={container.crew.id}
+                            className="sidebar-crew-name">
+                            {container.crew.name}
+                          </NavItem>
+                        </LinkContainer>
+                      </MenuItem>
                     );
                   })}
-                </Nav>
+                </NavDropdown>
+              </Nav>
 
-                {this.props.userMemberCrews && this.props.userMemberCrews.length === 0 ? <p className="sidebar-empty-crews-msg"><Glyphicon glyph="info-sign" /> <em>Just getting started or looking to find that next crew for you? Try our browse or search features to find some crews to join today!</em></p> : null}
-            </Col>
-          </Row>
-        </Tab.Container>
+              {this.props.userMemberCrews && this.props.userMemberCrews.length === 0 ? <p className="sidebar-empty-crews-msg"><Glyphicon glyph="info-sign" /> <em>Just getting started or looking to find that next crew for you? Try our browse or search features to find some crews to join today!</em></p> : null}
+            </Navbar>
+          </Col>
+        </Row>
       </div>
     )
   }
