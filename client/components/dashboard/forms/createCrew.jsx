12d8f1aaf935b667fedd6ed0ab9266572deadadf
Meghana <meghana94987@gmail.com>
2017-11-07 06:34:39 +0530

    Cloudinary shifted to backend (#234)
    * upload to cloudinary
    
    * install multer
    
    * moved cloudinary to server
    
    * get rid of configfile
    
    * Update package.json
    
    * Update index.js
    
    * Update createCrew.jsx

--- client/components/dashboard/forms/createCrew.jsx
+++ client/components/dashboard/forms/createCrew.jsx
@@ -7,106 +7,98 @@ import Dropzone from 'react-dropzone';
 import cloudinary from 'cloudinary-core';
 import request from 'superagent';
 
-import {cloud_name, CLOUDINARY_UPLOAD_PRESET, CLOUDINARY_UPLOAD_URL} from './configfile.js';
-
 export default class CreateCrew extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      uploadedFileCloudinaryUrl: ''
+      url: '',
+      name: '',
+      description: '',
+      image: null
     };
     // props contain name and unique details of user, so we can keep track of who created this crew  and store it in database accordingly
 
     this.handleSubmit = (e) => {
       e.preventDefault();
-      console.log(this.name);
-      console.log(this.desc);
-      console.log(this.img);
-      var obj = {
-        name: this.name.value,
-        description: this.description.value,
-        image: this.state.uploadedFileCloudinaryUrl
+      var formData = new FormData();
+      formData.append("picture", this.state.image);
+
+      var options = {
+        method: 'POST',
+        body: formData
       };
-      this.refs.form.reset();
-      console.log(obj);
-      PostCrew(obj, this.props.user.id, function (err, data) {
-        if (err) {
-          console.log('error in posting');
-        }
-        if (data) {
-          props.getCurrentCrews(props.user.id);
-          console.log(data, 'data from posting');
-          //document.getElementById("course-form").reset();
-        }
-      });
-    };
-  }
 
+      fetch('/image', options)
+        .then((res) => res.json())
+        .then((data) => {
+          // console.log(data, 'data in line 83')
+          this.setState({url: data.message})
+          var obj = {
+            name: this.state.name,
+            description: this.state.description,
+            image: this.state.url
+          }
 
+          PostCrew(obj, this.props.user.id, function (err, data) {
+            if (err) {
+              console.log('error in posting');
+            }
+            if (data) {
+              props.getCurrentCrews(props.user.id);
+              console.log(data, 'data from posting');
+              //document.getElementById("course-form").reset();
+            }
+          });
+        })
+        .catch((errors) => {
+          console.log('Login Error: ', errors);
+        })
+    }
 
-  image(e) {
-    this.img = e.target.value;
-    console.log(this.img, 'thisimg');
-  }
-
-  onImageDrop(files) {
-    this.setState({
-      uploadedFile: files[0]
-    }, function(err, done) {
-      if (done) {
-        console.log(this.state.uploadedFile);
-      }
-    });
-    this.handleImageUpload(files[0]);
-  }
-
-  handleImageUpload(file) {
-    let upload = request.post(CLOUDINARY_UPLOAD_URL)
-      .field('upload_preset', CLOUDINARY_UPLOAD_PRESET)
-      .field('file', file);
-
-    upload.end((err, response) => {
-      if (err) {
-        console.log(err);
-      }
-
-      if (response.body.secure_url !== '') {
-        this.setState({
-          uploadedFileCloudinaryUrl: response.body.secure_url
-        });
-      }
-    });
   }
 
   render() {
     return (
       <div>
-        <form onSubmit={this.handleSubmit} id="course-form" ref="form">
-          <FormGroup>
-            <FormControl type="text" placeholder="Enter the name of Crew" name="crewname" inputRef={ref => this.name = ref} defaultValue={this.props.name}/><br/>
-            <FormControl componentClass="textarea" placeholder="enter description" inputRef={ref => this.description = ref} defaultValue={this.props.desc}/><br/>
-            <Dropzone
-              muliple="false"
-              accept="image/*"
-              onDrop={this.onImageDrop.bind(this)}>
-
-              {this.state.uploadedFileCloudinaryUrl !== '' ?
-                <div>
-                  <p>{this.state.uploadedFile.name}</p>
-                  <Image cloudName={cloud_name} publicId={this.state.uploadedFileCloudinaryUrl} width="200" crop="scale"/>
-                </div> : <div>
-                  <p>Drop an image or click to select a file to upload.</p>
-                </div>}
-
-            </Dropzone>
-            <Button type="submit">
-               Create/Update
-            </Button>
-          </FormGroup>
-        </form>
+      <form onSubmit={this.handleSubmit}>
+        <FormControl type="text" name="name" value={this.state.name} placeholder="enter name" onChange={(e) => this.setState({name: e.target.value})}/> <br/>
+        <FormControl type="text" name="description" value={this.state.description} placeholder="description" onChange={(e) => this.setState({description: e.target.value})}/> <br/>
+        <FormControl type="file" name="pic" onChange={(e) => this.setState({image: e.target.files[0]})}/><br/>
+        <button>submit</button>
+      </form>
       </div>
     );
   }
 }
+
+
+// <form onSubmit={this.handleSubmitt} id="course-form" ref="form">
+          // <input type="text" name="name" value={this.state.name} placeholder="enter name" onChange={(e) => this.setState({name: e.target.value})}/>
+          // <input type="text" name="description" value={this.state.description} placeholder="description" onChange={(e) => this.setState({description: e.target.value})}/>
+          // <input type="file" name="image" onChange={(e) => this.setState({image: e.target.files[0]})} />
+          // <button>Submit</button>
+//        </form>
+
+//<FormControl type="text" placeholder="Enter the name of Crew" name="crewname" inputRef={ref => this.name = ref} defaultValue={this.props.name}/><br/>
+//           <FormControl componentClass="textarea" placeholder="enter description" inputRef={ref => this.description = ref} defaultValue={this.props.desc}/><br/>
+//            <Dropzone
+              // muliple="false"
+              // accept="image/*"
+              // onDrop={this.onImageDrop.bind(this)}>
+
+            //   {this.state.uploadedFileCloudinaryUrl !== '' ?
+            //     <div>
+            //       <p>{this.state.uploadedFile.name}</p>
+            //       <Image cloudName={cloud_name} publicId={this.state.uploadedFileCloudinaryUrl} width="200" crop="scale"/>
+            //     </div> : <div>
+            //       <p>Drop an image or click to select a file to upload.</p>
+            //     </div>}
+
+            // </Dropzone>
+            // <input type="file" onChange={this.handleFormData}/>
+//            <Button type="submit">
+//               Create/Update
+//            </Button>
+
 
 // <Image cloudName="sarikonda" publicId="flower" width="300" crop="scale"/>
