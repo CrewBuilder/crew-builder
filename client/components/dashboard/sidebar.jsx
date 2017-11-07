cb8f06772699d753744f6c791dd7623012d93b7f
Zachary Biernat <zackbiernat@gmail.com>
2017-11-07 12:54:05 -0800

    Fixed rebase

--- client/components/dashboard/sidebar.jsx
+++ client/components/dashboard/sidebar.jsx
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 
 import { LinkContainer } from 'react-router-bootstrap';
 
+<<<<<<< HEAD
 import { MenuItem, NavDropdown, Navbar, Row, Col, Nav, NavItem, Tab, TagContainer, TabContent, TabPane, Label, Button, Glyphicon } from 'react-bootstrap';
+=======
+import { MenuItem, NavDropdown, NavBar, Row, Col, Nav, NavItem, Tab, TagContainer, TabContent, TabPane, Label, Button, Glyphicon } from 'react-bootstrap';
+>>>>>>> Styled sidebar a little
 
 export default class Sidebar extends Component {
 
@@ -57,6 +61,7 @@ export default class Sidebar extends Component {
                     );
                   })}
                 </Nav>
+                {this.props.userMemberCrews && this.props.userMemberCrews.length === 0 ? <p className="sidebar-empty-crews-msg"><Glyphicon glyph="info-sign" /> <em>Just getting started or looking to find that next crew for you? Try our browse or search features to find some crews to join today!</em></p> : null}
               </span>
             </Col>
           </Row>
