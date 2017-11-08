3670b42080b05e07c93e29803a656201764795de
Edward Plato <edplatomail@gmail.com>
2017-11-07 16:39:01 -0800

    cleanup uploads

--- client/components/dashboard/forms/configfile.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  cloud_name: 'sarikonda',
-  CLOUDINARY_UPLOAD_PRESET: 'rqsqai1h',
-  CLOUDINARY_UPLOAD_URL: 'https://api.cloudinary.com/v1_1/sarikonda/upload',
-  Image_Url: "https://res.cloudinary.com/sarikonda/w_300,h_200,c_crop/"
-};
