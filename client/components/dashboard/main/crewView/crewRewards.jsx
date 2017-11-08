63fe686a2cae056b1864613a25b3ce1625505e7b
Zachary Biernat <zackbiernat@gmail.com>
2017-11-07 20:36:35 -0800

    Added request to claim reward (#267)
    * Added request to claim reward
    
    * Added consideration for Reward 'limit'

--- client/components/dashboard/main/crewView/crewRewards.jsx
+++ client/components/dashboard/main/crewView/crewRewards.jsx
@@ -1,6 +1,7 @@
 // This component renders a list of rewards to be claimed
 import React, { Component } from 'react';
-import { Modal, ListGroup, ListGroupItem, Button } from 'react-bootstrap';
+import { Alert, Modal, ListGroup, ListGroupItem, Button, Form, FormControl, FormGroup, ControlLabel } from 'react-bootstrap';
+import { claimReward } from '../../../utils/requests.jsx';
 
 
 export default class CrewRewards extends Component {
@@ -10,7 +11,10 @@ export default class CrewRewards extends Component {
     this.state = {
       selectedReward: props.currentCrewRewards,
       showModal: false,
-      crew: null
+      crew: null,
+      email: '',
+      alertVisible: false,
+      successVisible: false
     };
 
     this.handleSelectReward = (reward) => {
@@ -25,37 +29,89 @@ export default class CrewRewards extends Component {
 
     this.claimReward = () => {
       this.setState({showModal: false});
-      let reward = this.state.selectedReward.id;
-      //TODO: Do something when they claim a reward
+      let reward_id = this.state.selectedReward.id;
+      let email = this.state.email;
+      let availablePoints = this.state.crew.points;
+      let points = this.state.selectedReward.points;
+      let user_id = this.props.userId;
+      let crew_id = this.state.crew.crew.id;
+      let limit = this.state.selectedReward.limit;
+      if (availablePoints >= points && limit > 0) {
+        ClaimReward(reward_id, email, user_id, crew_id, points, (err, res) => {
+          if (res) {
+            this.setState({
+              successVisible: true
+            });
+          }
+        });
+      } else {
+        this.setState({
+          alertVisible: true,
+        });
+      }
+    };
+
+    this.handleAlertDismiss = () => {
+      this.setState({
+        alertVisible: false,
+        successVisible: false
+      });
     };
   }
 
   render() {
-    return (
-      <div>
+    if (this.state.alertVisible) {
+      return (
+        <Alert bsStyle="danger" onDismiss={this.handleAlertDismiss}>
+          <h4>You need more points!</h4>
+          <p>Complete tasks to earn enough points for this sweet reward</p>
+          <p>
+            <Button onClick={this.handleAlertDismiss}>Ok</Button>
+          </p>
+        </Alert>
+      );
+    } else if (this.state.successVisible) {
+      return (
+        <Alert bsStyle="success" onDismiss={this.handleAlertDismiss}>
+          <h4>You earned {this.state.selectedReward.name}!</h4>
+          <p>Check your email for instructions to redeem your reward!</p>
+          <p>
+            <Button onClick={this.handleAlertDismiss}>Ok</Button>
+          </p>
+        </Alert>
+      );
+    } else {
+      return (
         <div>
-          <ListGroup>
-            {this.props.currentCrewRewards.map((reward, i) => <ListGroupItem onClick={() => this.handleSelectReward(reward)} key={i}>{reward.name}</ListGroupItem>)}
-          </ListGroup>
+          <div>
+            <ListGroup>
+              {this.props.currentCrewRewards.map((reward, i) => <ListGroupItem onClick={() => this.handleSelectReward(reward)} key={i}>{reward.name}</ListGroupItem>)}
+            </ListGroup>
+          </div>
+          <div>
+            <Modal show={this.state.showModal} onHide={this.closeModal}>
+              <Modal.Header closeButton>
+                <Modal.Title>{this.state.selectedReward.name}</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <h4>Points: {this.state.selectedReward.points}</h4>
+                <h4>Remaining: {this.state.selectedReward.limit}</h4>
+                <p>{this.state.selectedReward.description}</p>
+                <Form onSubmit={this.handleSubmit}>
+                  <FormGroup>
+                    <ControlLabel>Email: </ControlLabel>
+                    <FormControl type="text" placeholder="Your email" value={this.state.email} onChange={(e) => this.setState({email: e.target.value})}/>
+                  </FormGroup>
+                </Form>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button onClick={this.claimReward}>Claim this reward</Button>
+                <Button onClick={this.closeModal}>Close</Button>
+              </Modal.Footer>
+            </Modal>
+          </div>
         </div>
-        <div>
-          <Modal show={this.state.showModal} onHide={this.closeModal}>
-            <Modal.Header closeButton>
-              <Modal.Title>{this.state.selectedReward.name}</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <h4>Points: {this.state.selectedReward.points}</h4>
-              <p>{this.state.selectedReward.description}</p>
-            </Modal.Body>
-            <Modal.Footer>
-              <Button onClick={this.claimReward}>Claim this reward</Button>
-              <Button onClick={this.closeModal}>Close</Button>
-            </Modal.Footer>
-          </Modal>
-        </div>
-      </div>
-    );
-
+      );
+    }
   }
-
 }
