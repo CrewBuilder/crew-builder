aafc62ac2ee9281ad0adcc73b4fa6fea4858bd4b
ionajewel <ipjwilli@gmail.com>
2017-11-08 16:53:22 -0800

    renamed utils to controllers

--- server/db/controllers/crewHelpers.js
+++ server/db/controllers/crewHelpers.js
@@ -1,85 +1,68 @@
 const db = require('../index.js');
 
 // Allows crew creation
-module.exports = {
-  postCrew(req, res) {
-    let crew = {
-      name: req.body.name,
-      description: req.body.description,
-      image: req.body.image
-    };
-    let user_id = req.body.user_id;
-    let newCrew;
-    db.crew
-      .create(crew)
-      .then(crew => {
-        newCrew = crew;
-        return db.user_crew.create({
-          crew_id: crew.id,
-          user_id: user_id,
-          role: 'leader'
+exports.postCrew = (crewData, userId, cb) => {
+  //this query has been tested OK
+  let user = userId;
+  db.crew.create(crewData)
+    .then(crew => {
+      return crew.id;
+    })
+    .then(crew_id => {
+      db.user_crew.create({
+        crew_id: crew_id,
+        user_id: user,
+        points: 0,
+        achievement: 'none',
+        role: 'leader'
+      })
+        .then(crew => {
+          cb(null, crew);
         });
-      })
-      .then(crew => {
-        res.status(200).send(newCrew);
-      })
-      .catch(err => {
-        res.status(500).send(err);
-      });
-  },
+    })
+    .catch(err => {
+      return cb(err, null);
+    });
+};
 
-  searchCrews(req, res) {
-    qs = req.query.qs || null;
-    if (qs) {
-      qs = `%${qs}%`;
-      return db.crew
-        .findAll({
-          where: {
-            $or: {
-              name: {
-                $iLike: qs
-              },
-              description: {
-                $iLike: qs
-              }
-            }
+exports.searchCrews = (qs) => {
+  if (qs) {
+    qs = `%${qs}%`;
+    return db.crew.findAll({
+      where: {
+        $or: {
+          name: {
+            $iLike: qs
+          },
+          description: {
+            $iLike: qs
           }
-        })
-        .then(found => res.status(200).send(found))
-        .catch(err => res.status(500).send(err));
-    } else {
-      return db.crew
-        .findAll()
-        .then(found => res.status(200).send(found))
-        .catch(err => res.status(500).send(err));
-    }
-  },
-
-  editCrew(req, res) {
-    return db.crew
-      .findOne({
-        where: {
-          id: req.query.crew_id
         }
-      })
-      .then(found => {
-        return found.update(req.body);
-      })
-      .then(updated => res.status(200).send(updated))
-      .catch(err => {
-        console.log(err);
-        res.status(500).send(err);
-      });
-  },
-
-  deleteCrew(req, res) {
-    db.crew
-      .destroy({
-        where: {
-          id: req.query.crew_id
-        }
-      })
-      .then(destroyed => res.sendStatus(202))
-      .catch(err => res.status(500).send(err));
+      }
+    });
+  } else {
+    return db.crew.findAll();
   }
+};
+
+exports.editCrew = (req, res) => {
+  return db.crew
+    .update(req.body, {
+      where: {
+        id: req.query.crew_id
+      }
+    })
+    .then(updated => res.status(200).send(updated))
+    .catch(err => res.status(500).send(err));
+};
+
+exports.deleteCrew = (req, res) => {
+  db.crew
+    .destroy({
+      where: {
+        id: req.query.crew_id
+      }
+    })
+    .then(destroyed => res.sendStatus(202))
+    .catch(err => res.status(500).send(err));
 };
